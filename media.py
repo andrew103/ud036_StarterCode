804bf6bf1d7b250df3d71da2512aa5f009f04e81
Andrew Euredjian <andrew_103@rocketmail.com>
2017-06-23 13:42:45 -0700

    changes for PEP-8 compliance

--- media.py
+++ media.py
@@ -1,12 +1,8 @@
 class Movie():
-	"""
-	A class that stores information about a movie
-	"""
-	
+	"""A class that stores information about a movie"""
+
 	def __init__(self, title, description, poster, trailer):
-		"""
-		Initializes a movie object when the class is called
-		"""
+		"""Initializes a movie object when the class is called"""
 		self.title = title
 		self.description = description
 		self.poster = poster
